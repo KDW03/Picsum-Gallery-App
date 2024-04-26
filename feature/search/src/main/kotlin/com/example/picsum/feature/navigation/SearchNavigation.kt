3bc73c78d2e497c6c40cd00cb92d46f52c365930
KDW03 <asasa0628@gs.cwnu.ac.kr>
2024-04-26 22:35:20 +0900

    feat: Implement feature:searchRoute (#7)

---
+++ feature/search/src/main/kotlin/com/example/picsum/feature/navigation/SearchNavigation.kt
@@ -0,0 +1,24 @@
+package com.example.picsum.feature.navigation
+
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+
+const val searchRoute = "search_route"
+
+fun NavController.navigateToSearch(navOptions: NavOptions? = null) {
+    this.navigate(searchRoute, navOptions)
+}
+
+fun NavGraphBuilder.searchScreen(
+    onBackClick: () -> Unit,
+    onFeedClick: (Int) -> Unit,
+) {
+    composable(route = searchRoute) {
+        SearchRoute(
+            onBackClick = onBackClick,
+            onFeedClick = onFeedClick,
+        )
+    }
+}
