5222f341753a1efbb489549b4b0c74c4c3d80524
KDW03 <asasa0628@gs.cwnu.ac.kr>
2024-04-26 22:18:39 +0900

    feat: Implement PsApplication (#7)

---
+++ app/src/main/java/com/example/picsum/PsApplication.kt
@@ -0,0 +1,17 @@
+package com.example.picsum
+
+import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+import javax.inject.Provider
+
+@HiltAndroidApp
+class PsApplication : Application(), ImageLoaderFactory {
+
+    @Inject
+    lateinit var imageLoader: Provider<ImageLoader>
+
+    override fun newImageLoader(): ImageLoader = imageLoader.get()
+}
