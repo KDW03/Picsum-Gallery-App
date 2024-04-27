b7da49d0e7a27d357922c72dd1c1e3e8903071c2
KDW03 <asasa0628@gs.cwnu.ac.kr>
2024-04-27 14:26:18 +0900

    feat: Implement SearchScreen (#12)

--- feature/search/src/main/kotlin/com/example/picsum/feature/navigation/SearchNavigation.kt
+++ feature/search/src/main/kotlin/com/example/picsum/feature/navigation/SearchNavigation.kt
@@ -4,6 +4,8 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.example.picsum.feature.SearchRoute
+
 
 const val searchRoute = "search_route"
 
