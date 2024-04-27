feedc69eb6aea5358ac8c8b6d128f6d58817fc41
KDW03 <asasa0628@gs.cwnu.ac.kr>
2024-04-27 15:06:23 +0900

    feat: Implement SettingsScreen (#15)

--- feature/settings/src/main/kotlin/com/example/picsum/feature/settings/navigation/settingsNavigationRoute.kt
+++ feature/settings/src/main/kotlin/com/example/picsum/feature/settings/navigation/settingsNavigationRoute.kt
@@ -4,6 +4,7 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import com.example.picsum.feature.settings.SettingsRoute
 
 const val settingsNavigationRoute = "settings_route"
 fun NavController.navigateToSettings(navOptions: NavOptions? = null) {
